b6714f1aa6284325b3dae591c279dd852ba3c1e8
Jonathan-dev2002 <jonathandoillon2002@gmail.com>
2026-03-26 23:44:24 +0700

    feat: add role description

--- marketplace/src/main/java/com/jo/marketplace/constant/RoleConstants.java
+++ marketplace/src/main/java/com/jo/marketplace/constant/RoleConstants.java
@@ -4,4 +4,8 @@ public class RoleConstants {
     public static final String ADMIN = "ADMIN";
     public static final String SELLER = "SELLER";
     public static final String BUYER = "BUYER";
+
+    public static final String ADMIN_DESC = "ผู้ดูแลระบบสูงสุดของแพลตฟอร์ม";
+    public static final String SELLER_DESC = "เจ้าของร้านค้า";
+    public static final String BUYER_DESC = "ผู้ใช้งาน/ผู้ซื้อทั่วไป";
 }
