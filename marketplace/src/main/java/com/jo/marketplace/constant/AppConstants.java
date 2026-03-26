96acf2b7d14ce29b90f932cb73665e646488b693
Jonathan-dev2002 <jonathandoillon2002@gmail.com>
2026-03-26 23:42:55 +0700

    feat: add app constant file

--- marketplace/src/main/java/com/jo/marketplace/constant/AppConstants.java
+++ marketplace/src/main/java/com/jo/marketplace/constant/AppConstants.java
@@ -3,6 +3,12 @@ package com.jo.marketplace.constant;
 import java.util.UUID;
 
 public class AppConstants {
-    // ใช้ UUID จำลองตัวนี้สำหรับสิทธิ์ระดับ Platform (สิทธิ์กลางที่ไม่ได้ผูกกับร้านค้าใดๆ)
+
     public static final UUID PLATFORM_SHOP_ID = UUID.fromString("00000000-0000-0000-0000-000000000000");
+
+    // ข้อมูลร้านค้า Platform
+    public static final String PLATFORM_SHOP_NAME = "Platform Global";
+    public static final String PLATFORM_SHOP_SLUG = "platform-global";
+    public static final String PLATFORM_SHOP_DESC = "System defined shop for platform-level roles.";
+
 }
